f424a4cd4cef21fd710acd9edaf06d16de401ea4
shivamjha296 <sjha13780@gmail.com>
2025-02-22 15:45:02 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,28 +9,36 @@ function Footer() {
           <h3>FileConverter</h3>
           <p>Convert and compress your files easily with our free online tool.</p>
         </div>
-        
+
         <div className="footer-section">
           <h4>Quick Links</h4>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/#home">Home</a></li>
+            <li><a href="/#features">Features</a></li>
+            <li><a href="/#about">About</a></li>
+            <li><a href="/#contact">Contact</a></li>
           </ul>
         </div>
-        
+
         <div className="footer-section">
           <h4>Connect With Us</h4>
           <div className="social-links">
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-linkedin"></i></a>
-            <a href="#"><i className="fab fa-github"></i></a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
           </div>
         </div>
       </div>
-      
+
       <div className="footer-bottom">
         <p>&copy; 2024 FileConverter. All rights reserved.</p>
       </div>
@@ -38,4 +46,4 @@ function Footer() {
   );
 }
 
-export default Footer; 
+export default Footer;
